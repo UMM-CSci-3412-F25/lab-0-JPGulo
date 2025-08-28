016cd52bae4b6658244ca989e0c950066dc623b9
JPGulo <gulon008@morris.umn.edu>
2025-08-28 11:13:07 -0500

    Script 2 Finished
    Made a variable/argument ARCHIVE to receive the archive name. Then make a var DIRNAME that gets the base name (ex. big_dir not Big_dir.tgz). Under var SCRATCH made a temporary directory and extracted the archive into temp directory. Used grep to find all files containing "DELETE ME!" in extracted directory and removed. Then changed directory to the temp directory SCRATCH to create a new archive named cleaned_file_name. Used HERE var to use the current working directory in creating the new archive.

---
+++ cleaning/big_clean.sh
@@ -0,0 +1,16 @@
+
+
+ARCHIVE="$1"
+
+DIRNAME=$(basename "$ARCHIVE" .tgz)
+
+HERE=$(pwd)
+
+SCRATCH=$(mktemp -d)
+
+tar -xzf "$ARCHIVE" -C "$SCRATCH"
+
+grep -rl 'DELETE ME!' "$SCRATCH/$DIRNAME" | xargs rm -f
+
+cd "$SCRATCH"
+tar -czf "$HERE/cleaned_$ARCHIVE" "$DIRNAME"
